147a9a0339373576c7e57ffa64c8651e4a943f5a
Steven Than <steventhan11@gmail.com>
2017-06-13 12:49:21 -0400

    StockPriceNotFound now extends RuntimeException, elimiated the need to handle it

--- src/StockPriceNotFound.java
+++ src/StockPriceNotFound.java
@@ -1,7 +1,7 @@
 /**
  * This class represents an exception that can be thrown when a Stock's price cannot be found.
  */
-public class StockPriceNotFound extends Exception {
+public class StockPriceNotFound extends RuntimeException {
   public StockPriceNotFound(String msg) {
     super(msg);
   }
