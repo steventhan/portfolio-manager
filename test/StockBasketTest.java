32f7fe94f3f6cc7c6324144c79a9cbcef7177bbd
Steven Than <steventhan11@gmail.com>
2017-06-14 11:52:59 -0400

    started adding more tests for StockBasket

--- test/StockBasketTest.java
+++ test/StockBasketTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -10,33 +11,31 @@ import java.util.Map;
  * JUnit tests for StockBasket.
  */
 public class StockBasketTest {
-
-  NewStockRetriever retriever = WebRetrieverSingleton.getInstance();
-  IStock emptyBasket;
-  IStock fullBasket;
+  StockBasket sb1;
+  StockBasket sb2;
+  StockBasket sb3;
+  StockBasket sb4;
 
   @Before
   public void setup() throws Exception {
-    emptyBasket = new StockBasket();
+    this.sb1 = new StockBasket();
 
     // using HashMap because, order doesn't matter
     Map<StockSingle, Integer> temp = new HashMap<>();
-    temp.put(new StockSingle("MMM"), 32800);
-    temp.put(new StockSingle("ATVI"), 357800);
-    temp.put(new StockSingle("ADBE"), 101300);
-    temp.put(new StockSingle("A"), 26300);
-    temp.put(new StockSingle("VZ"), 387500);
-    temp.put(new StockSingle("UPS"), 57000);
+    temp.put(new StockSingle("MMM"), 3);
+    temp.put(new StockSingle("ATVI"), 4);
+    temp.put(new StockSingle("ADBE"), 5);
+    temp.put(new StockSingle("A"), 6);
+    temp.put(new StockSingle("VZ"), 7);
+    temp.put(new StockSingle("UPS"), 8);
 
-    fullBasket = new StockBasket(temp);
-
+    this.sb2 = new StockBasket(temp);
+    this.sb3 = new StockBasket();
+    this.sb4 = new StockBasket();
   }
-
-
 
   @Test
   public void testConstructorLegal() {
-
   }
 
   @Test
@@ -47,13 +46,16 @@ public class StockBasketTest {
   @Test
   public void testGetPriceOnDay() throws Exception {
 
-    System.out.format("%f", fullBasket.getPriceOnDay("2017-06-13"));
-
-    //TODO: 11:52am  2017-06-13 returns 0.0
   }
 
   @Test
   public void testTrendsUp() throws Exception {
+    Assert.assertTrue(this.sb2.trendsUp("2017-06-13", "2017-06-12"));
+  }
+
+  @Test
+  public void testTrendsUpIllegalDates() throws Exception {
+    Assert.assertTrue(this.sb2.trendsUp("2017-06-13", "2017-06-12"));
   }
 
 }
