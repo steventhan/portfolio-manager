8881fd299f22436d7a16063eeccedbe1809c3f22
Steven Than <steventhan11@gmail.com>
2017-06-14 20:36:35 -0400

    make abstract class public

--- src/StockAbstract.java
+++ src/StockAbstract.java
@@ -4,7 +4,7 @@ import custom.util.CustomDate;
  * This class represents an abstract stock which implements operations shared between any stock
  * classes.
  */
-abstract class StockAbstract implements IStock {
+public abstract class StockAbstract implements IStock {
 
   /**
    * Verifies the start date is before the end date, and the end date is not in the future.
