0d435ca98292e60070f1b18715470e756d272c55
matthiasdenu <denu.m@husky.neu.edu>
2017-06-19 13:22:35 -0400

    :thought_balloon: messing around with DrawLine

--- src/view/trader/DrawLine.java
+++ src/view/trader/DrawLine.java
@@ -3,6 +3,7 @@ package view.trader;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.swing.*;
@@ -84,18 +85,33 @@ class DrawPanel extends JPanel {
 
     //set color to black
     g2d.setColor(Color.BLACK);
-    g2d.drawLine(50, 50, 100, 100);
-
-    //set color to red
-    g2d.setColor(Color.RED);
-    g2d.drawLine(50, 150, 300, 200);
+    //TODO: Grow legend taller with added symbols
+    //TODO: Legend is 100px wide right now
+    //TODO: String padding is 15px
+//    g2d.drawLine(50, 400, 50, 450);
+//    g2d.drawLine(50, 450, 150, 450);
+//    g2d.drawLine(50, 400, 150, 400);
+//    g2d.drawLine(150, 400, 150, 450);
+    g2d.drawRect(50, 400, 100, 50);
 
     //write some text
     g2d.setColor(Color.BLACK);
-    g2d.drawString("Here is the string", 400, 400);
+    g2d.drawString("String 1", 100, 415);
+    g2d.drawString("String 2", 100, 430);
+    g2d.drawString("String 3", 100, 445);
 
+    //TODO: pick unused colors for newly added symbols
+    g2d.setColor(Color.RED);
+    g2d.drawLine(60, 415, 75, 415);
+    g2d.setColor(Color.ORANGE);
+    g2d.drawLine(60, 430, 75, 430);
+    g2d.setColor(Color.GREEN);
+    g2d.drawLine(60, 445, 75, 445);
+
+    g2d.setColor(Color.BLACK);
     for (Point p : points) {
       g2d.fillOval(p.x, p.y, 10, 10);
     }
   }
 }
+
