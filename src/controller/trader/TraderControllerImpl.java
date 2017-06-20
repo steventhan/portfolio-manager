b59a2df486ba0d2942d646bfc2f409f2c42a01c1
Steven Than <steventhan11@gmail.com>
2017-06-20 03:59:22 -0400

    added TraderController interface and its implementation

--- src/controller/trader/TraderControllerImpl.java
+++ src/controller/trader/TraderControllerImpl.java
@@ -1,7 +1,30 @@
 package controller.trader;
 
+import java.io.InputStreamReader;
+
+import model.trader.TraderModel;
+import view.trader.TraderView;
+
 /**
  * An implementation of TraderController interface.
  */
 public class TraderControllerImpl implements TraderController {
+  private TraderModel model;
+  private TraderView view;
+  private Readable in;
+
+  public TraderControllerImpl(TraderModel model, TraderView view) {
+    this(model, view, new InputStreamReader(System.in));
+  }
+
+  public TraderControllerImpl(TraderModel model, TraderView view, Readable in) {
+    this.model = model;
+    this.view = view;
+    this.in = in;
+  }
+
+  @Override
+  public void start() throws Exception {
+
+  }
 }
--- src/view/trader/TradeTextView.java
+++
@@ -1,7 +0,0 @@
-package view.trader;
-
-/**
- * Created by steven on 17/06/2017.
- */
-public class TradeTextView implements DrawStock {
-}
