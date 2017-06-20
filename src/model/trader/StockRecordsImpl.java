95fff7a7637f381664de50f6903f51f65001a75e
matthiasdenu <denu.m@husky.neu.edu>
2017-06-20 13:41:41 -0400

    :construction: StockRecords.java

--- src/model/trader/StockRecordsImpl.java
+++ src/model/trader/StockRecordsImpl.java
@@ -1,7 +1,13 @@
 package model.trader;
 
+import java.util.List;
+
 /**
- * Class used to pass information from model to controller
+ * Class to keep track of what stocks/baskets should be plotted. Iterate through this to plot each
+ * item. Remove items when you no longer want them to be plotted.
  */
 public class StockRecordsImpl {
+  //TODO: pass StockRecordsImpl from model -> controller and possibly to view to plot
+  List<StockBasket> baskets;
+  List<StockSingle> stocks;
 }
