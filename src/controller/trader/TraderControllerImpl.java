1c5428bb19b7a2eb9bb6adcbc741b87c05af3ef5
Steven Than <steventhan11@gmail.com>
2017-06-20 17:01:13 -0400

    added method body for TraderControllerImpl:start()

--- src/controller/trader/TraderControllerImpl.java
+++ src/controller/trader/TraderControllerImpl.java
@@ -1,6 +1,8 @@
 package controller.trader;
 
 import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.Scanner;
 
 import model.trader.TraderModel;
 import view.trader.TraderView;
@@ -25,9 +27,52 @@ public class TraderControllerImpl implements TraderController {
 
   @Override
   public void start() throws Exception {
-    // start while loop
-    // this.view.printMenu();
-    // read from input here
-    // repaint command view.repaint() at key times
+    Scanner sc = new Scanner(this.in);
+    Map<String, Integer> basket;
+
+    while (true) {
+      this.view.printMenu(this.model.getMenuOptions());
+      switch (sc.nextLine()) {
+        case "c":
+          this.view.append("Enter stock basket name: ");
+          try {
+            this.model.createStockBasket(sc.nextLine());
+          } catch (IllegalArgumentException e) {
+            this.view.append(e.getMessage() + "\n");
+          }
+          break;
+
+        case "a":
+          this.view.append("Enter basket name to add: ");
+          String basketName = sc.nextLine();
+
+          this.view.append("Enter stock symbol: ");
+          String symbol = sc.nextLine();
+          this.view.append("Amount: ");
+          int amount = Integer.valueOf(sc.nextLine());
+
+          this.model.addStockToBasket(basketName, symbol, amount);
+          break;
+
+        case "p":
+          this.view.append("Enter basket name to print: ");
+          basket = this.model.getBasketContentByName(sc.nextLine());
+          if (basket == null) {
+            this.view.append("Basket not found\n");
+            break;
+          }
+          this.view.printBasket(basket);
+          this.view.append("\n");
+          break;
+
+        case "g":
+          this.start();
+          break;
+
+        case "q":
+          this.view.exit();
+      }
+      this.view.append("\n");
+    }
   }
 }
