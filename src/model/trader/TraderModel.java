b0abaed358be524b78927c92edb671cac17039cb
Steven Than <steventhan11@gmail.com>
2017-06-23 22:14:37 -0400

    duplicated javadocs from interface to its implementation

--- src/model/trader/TraderModel.java
+++ src/model/trader/TraderModel.java
@@ -11,6 +11,7 @@ public interface TraderModel {
    * Adds stock to a basket.
    * It is possible to remove stocks by sending a negative number. It is also possible to store a
    * negative number of stocks in a basket.
+   *
    * @param name of basket.
    * @param symbol of stock.
    * @param numShare number of shares of stock to add to the basket.
@@ -22,6 +23,7 @@ public interface TraderModel {
    * Creates a stock basket.
    * A previously created stock basket can be overwritten by creating a new stock basket of the
    * same name.
+   *
    * @param name of basket.
    * @throws Exception described by its message.
    */
@@ -29,6 +31,7 @@ public interface TraderModel {
 
   /**
    * Get the contents of a basket.
+   *
    * @param sbName stock basket name.
    * @return representation of the stock baskets contents, i.e. the stock names and the shares per
    *         stock.
@@ -37,12 +40,14 @@ public interface TraderModel {
 
   /**
    * Returns a representation of all the baskets currently in the model.
+   *
    * @return a representation of all the baskets currently in the model.
    */
   Map<String, Map<String, Integer>> getBaskets();
 
   /**
    * Returns highest price of all the stock entities of all the days in the range.
+   *
    * @return highest price.
    */
   double getHighestPrice();
@@ -50,6 +55,7 @@ public interface TraderModel {
   /**
    * Returns a representation of the moving averages for each stock or basket present for each day
    * in the given range.
+   *
    * @param fromDate date range start.
    * @param toDate date range end.
    * @param days number of days to average over.
@@ -63,6 +69,7 @@ public interface TraderModel {
   /**
    * Returns a representation of the moving averages for a stock or basket present for each day
    * in the given range.
+   *
    * @param stockOrBasketName stock or basket name.
    * @param fromDate date range start.
    * @param toDate date range end.
@@ -75,6 +82,7 @@ public interface TraderModel {
 
   /**
    * Return the plot data for all stocks or baskets currently stored in the model.
+   *
    * @param fromDate date range start.
    * @param toDate date range end.
    * @return the plot data for all stocks or baskets currently stored in the model.
@@ -84,6 +92,7 @@ public interface TraderModel {
 
   /**
    * Return the plot data for a stock or basket currently stored in the model.
+   *
    * @param stockOrBasketName stock basket or name.
    * @param fromDate date range start.
    * @param toDate date range end.
@@ -101,6 +110,7 @@ public interface TraderModel {
   /**
    * Returns true if the stock entity trends up, given the specified dates. This is a naive
    * algorithm that just looks at the difference between the values at the start and end dates.
+   *
    * @param name stock entity name.
    * @param fromDate date range start.
    * @param toDate date range end.
